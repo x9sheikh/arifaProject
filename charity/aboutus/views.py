54c4db4ca471e19674e995805bde49a5d4867b9b
x9sheikh <x9sheikh.usama@gmail.com>
2019-10-27 17:32:14 +0500

    FrontEnd Added Successfully

--- charity/aboutus/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import *
-
-# Create your views here.
-def aboutus_view(request):
-    context = {
-
-    }
-    return render(request, 'aboutus.html', context=context)
--- charity/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import *
-# Create your views here.
-def home_view(request):
-    context={
-
-    }
-    return render(request, 'base.html', context=context)
